2cba9a211e99bfad38c04e49d3dc0a1891c71242
Tomas Mleziva <mlezivat@gmail.com>
2017-11-25 15:50:15 +0100

    Source Logger

---
+++ src/RollbarLogger.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Mlezitom\NetteRollbar;
+
+use Nette\Security\IIdentity;
+use Rollbar\Rollbar;
+use Tracy\Debugger;
+use Tracy\ILogger;
+
+class Logger implements ILogger
+{
+    public $identity;
+
+    /**
+     * @param Rollbar $client
+     */
+    public function __construct($rollbarKey, $env, IIdentity $identity)
+    {
+        $this->identity = $identity;
+        Rollbar::init([
+            'access_token' => $rollbarKey,
+            'environment' => $env,
+        ]);
+        $this->bindToTracy();
+    }
+
+
+    /**
+     * @param mixed $value
+     * @param string $priority
+     */
+    public function log($value, $priority = null)
+    {
+        if (!$priority && is_object($value) && $value instanceof \Exception) {
+            $priority = self::EXCEPTION;
+        }
+        else if (!$priority) {
+            $priority = self::ERROR;
+        }
+
+        Rollbar::log($priority, $value, [
+            'identity' => $this->identity,
+        ]);
+    }
+
+    public function logInfo($value)
+    {
+        return $this->log($value, self::INFO);
+    }
+
+
+    public function bindToTracy()
+    {
+        Debugger::setLogger($this);
+    }
+}
